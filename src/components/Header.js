c1c109627f02f650866f24d6a5c09cd2a3507c12
Wanchana S <wanchana.job@hotmail.com>
2023-07-06 16:46:07 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+import { BsSunFill, BsMoonStarsFill } from "react-icons/bs";
+function Header(props) {
+  const { theme, setTheme } = props;
+  function ToggleTheme() {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+  return (
+    <header>
+      <div className="logo">
+        <span>Task Management</span>
+      </div>
+      <div className="theme-container">
+        <span>{theme === "light" ? "โหมดกลางวัน" : "โหมดกลางคืน"}</span>
+        <span className="icon" onClick={ToggleTheme}>
+          {theme === "light" ? <BsSunFill /> : <BsMoonStarsFill />}
+        </span>
+      </div>
+    </header>
+  );
+}
+export default Header;
